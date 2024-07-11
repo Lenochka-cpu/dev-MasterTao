7e1f9708665fbcd974b3841d0c625687451b4ed7
Lenochka-cpu <lookovitza2019@gmail.com>
2024-07-11 22:09:59 +0300

    feat: first-screen desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,13 +1,30 @@
-const burger = document.querySelector('.btn-burger');
+const burger = document.querySelector('.hamburger');
 const mobileContainer = document.querySelector('.mobile-container') 
 const body = document.querySelector('body')
 
 function toggleClass() {
     mobileContainer.classList.toggle('show')
-    burger.classList.toggle('active')
+    burger.classList.toggle('is-active')
     body.classList.toggle('overflow-hidden')
     
 }
 
 burger.addEventListener('click', toggleClass)
 
+const swiper = new Swiper('.banner-slider', {
+ 
+    loop: true,
+    spaceBetween: 50,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
