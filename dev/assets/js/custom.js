b49d69057d71bd4c97f36ba622d74b5fc3285bf2
Lenochka-cpu <lookovitza2019@gmail.com>
2024-08-03 17:59:37 +0300

    feat: calculation mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -35,3 +35,51 @@ const swiper = new Swiper('.banner-slider', {
     },
   } 
 });
+
+// const cats = [
+// {
+//   name:"Barsik",
+//   old: 3,
+//   color:"red",
+// }
+// ]
+
+// console.table(cats[0].name);
+
+// const halfsection = document.querySelector('.half-section h2')
+//  halfsection.innerHTML = cats[0].name
+
+// const fileInput = document.querySelector('#product-photo');
+// const fileName = document.querySelector('#file-name')
+
+// const h2 = document.querySelector('h2')
+// const titlesArray = document.querySelectorAll('h2')
+
+
+
+// titlesArray.forEach(item => {
+
+//   item.addEventListener('click', ()=> item.closest('.container').classList.add('bg-warning'))
+// })
+
+// fileInput.addEventListener('change', () => {
+//   fileName.innerHTML = fileInput.files[0].name;
+// });
+
+const typeFile = document.querySelectorAll('.custom-file')
+
+typeFile.forEach(item=>{
+  item.addEventListener('change', ()=> {
+     const fileName = item.closest('.control-box').querySelector('.file-name')
+
+     fileName.innerHTML = item.files[0].name;
+    })
+})
+// typeFile.forEach(item=>{
+//   item.addEventListener('change', ()=> {
+//    const fileName = item.closest('.control-box').querySelector('.file-name')
+
+//    fileName.innerHTML = 'имя файла'
+//   })
+
+// })
